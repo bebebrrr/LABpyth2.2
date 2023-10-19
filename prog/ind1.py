970906a6c8875e55356a82d26f56370a61d50c90
bebebrrr <kubanova04@gmail.com>
2023-10-19 16:30:42 +0300

    пример 1

---
+++ prog/ind1.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import sys
+if __name__=='__main__':
+    c=int(input("value c? "))
+    if c<0 and c>9:
+        print("Error!", file=sys.stderr)
+        exit(1)
+    elif c==1:
+        print("one")
+    elif c==2:
+        print("two")
+    elif c==3:
+        print("three")
+    elif c==4:
+        print("four")
+    elif c==5:
+        print("five")
+    elif c==6:
+        print("six")
+    elif c==7:
+        print("seven")
+    elif c==8:
+        print("eight")
+    else:
+        print("nine")
+    
