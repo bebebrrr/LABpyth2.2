cca81156e019ac3901ebda7a236974d67ee84d64
bebebrrr <kubanova04@gmail.com>
2023-10-30 12:43:02 +0300

    new

--- prog/ind3.py
+++ prog/ind3.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-from prettytable import PrettyTable
 
 if __name__=='__main__':
  
  a=int(input("a= "))
- myTable= PrettyTable(["funts", "kg"])
- #print("| Вес в фунтах | Вес в килограммах |")
+ print("--------------")
+ print("| funts | kg  |")
+ print("--------------")
  for i in range (1, a, 1):
   f=i*400
   l=f/1000
-  myTable.add_row(["{0}", "{1}".format(i,l)])
-  #print("| {0} | {1} |".format(i, l))
+  print("| {0}     | {1} |".format(i, l))
+  print("--------------")
