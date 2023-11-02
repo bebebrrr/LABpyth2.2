7a681f0b7789d105d8cf02e099b3b10ff4a0a3b5
bebebrrr <kubanova04@gmail.com>
2023-11-02 15:15:55 +0300

    individual 2

--- prog/ind2.py
+++ prog/ind2.py
@@ -1,11 +1,18 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+import math
+
+
 if __name__=='__main__':
     a=float(input("a= "))
+    while a==0:
+    
+        print("а должно быть больше 0!")
+        a=float(input("a= "))
     b=float(input("b= "))
     c=float(input("c= "))
     d=b**2+4*a*c
-    x1=(-b+math.sqrt(d))/2*a
-    x2=(-b-)/2*a
-    print("x1={x1}, x2={x2}")
+    x1=(-b+ math.sqrt(d))/2*a
+    x2=(-b- math.sqrt(d))/2*a
+    print(f"x1={x1}, x2={x2}")
